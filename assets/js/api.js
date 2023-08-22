df97eeeac131bf8a67407a919f191412fe3b5880
Bruno Moura Soares <brunomourasoares@gmail.com>
2023-08-21 22:27:48 -0300

    fix url

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-    const url = '/data/profile.json';
+    const url = '/js-developer-portifolio/data/profile.json';
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
